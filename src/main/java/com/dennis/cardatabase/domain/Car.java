9f10533ab56d52567b4dcd44694385256bf3d909
dennis <ezechidc@yahoo.com>
2018-09-08 10:42:12 +0100

    Car model added

---
+++ src/main/java/com/dennis/cardatabase/domain/Car.java
@@ -0,0 +1,83 @@
+package com.dennis.cardatabase.domain;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false, length = 512)
+    private long id;
+    private String brand, model, color, registrationNumber;
+    private int year, price;
+
+
+    public Car() {
+    }
+
+    public Car(String brand, String model, String color, String registrationNumber, int year, int price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registrationNumber = registrationNumber;
+        this.year = year;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
