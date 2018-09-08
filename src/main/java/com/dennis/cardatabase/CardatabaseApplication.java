ffcad6ed63baba5137253ba85f171cb822f69b64
dennis <ezechidc@yahoo.com>
2018-09-08 13:02:32 +0100

    added configureGlobal method to enable users from the database

--- src/main/java/com/dennis/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/dennis/cardatabase/CardatabaseApplication.java
@@ -1,9 +1,6 @@
 package com.dennis.cardatabase;
 
-import com.dennis.cardatabase.domain.Car;
-import com.dennis.cardatabase.domain.CarRepository;
-import com.dennis.cardatabase.domain.Owner;
-import com.dennis.cardatabase.domain.OwnerRepository;
+import com.dennis.cardatabase.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,6 +14,9 @@ public class CardatabaseApplication {
 
     @Autowired
     private OwnerRepository orepository;
+
+    @Autowired
+    private UserRepository urepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
@@ -38,6 +38,15 @@ public class CardatabaseApplication {
                     "SSJ-3002", 2014, 29000,owner2));
             repository.save(new Car("Toyota", "Prius", "Silver",
                     "KKO-0212", 2018, 39000,owner1));
+
+            // username: user password: user
+            urepository.save(new User("user",
+                    "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi",
+                    "USER"));
+            // username: admin password: admin
+            urepository.save(new User("admin",
+                    "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG",
+                    "ADMIN"));
         };
     }
 }
--- src/main/java/com/dennis/cardatabase/SecurityConfig.java
+++ src/main/java/com/dennis/cardatabase/SecurityConfig.java
@@ -1,33 +1,30 @@
 package com.dennis.cardatabase;
 
+import com.dennis.cardatabase.service.UserDetailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailServiceImpl userDetailService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-    }
-
-    @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password("user")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
     }
 }
